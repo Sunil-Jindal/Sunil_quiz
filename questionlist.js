1cac461bc993a77aa1bbe03622cb3bbc11def367
Sunil Jindal <90545957+Sunil-Jindal@users.noreply.github.com>
2022-10-02 12:00:52 +0530

    Update questionlist.js

--- questionlist.js
+++ questionlist.js
@@ -53,105 +53,105 @@ const numberOfQuestion = [
             'wrap',
         ]
     },
-    // {
-    //     questionNumber : 6,
-    //     question: 'Can we define the direction of the text via a CSS property?',
-    //     correctAnswer:'No, its not possible.',
-    //     option:[
-    //         'Yes its possible.',
-    //         'No, its not possible.',
-    //         'may be possible.',
-    //         'A true',
-    //     ]
-    // },{
-    //     questionNumber : 7,
-    //     question: 'Choose the correct HTML tag for a large title.',
-    //     correctAnswer:'H1',
-    //     option:[
-    //         'H1',
-    //         'Heading',
-    //         'Head',
-    //         'H6',
-    //     ]
-    // },{
-    //     questionNumber : 8,
-    //     question: 'In HTML, Uniform Resource Locator (URL) is used ',
-    //     correctAnswer:'To identify a name or a resource on the Internet.',
-    //     option:[
-    //         'To create a frame document.',
-    //         'To create an image map in a web page.',
-    //         'To customize the image in a web page.',
-    //         'To identify a name or a resource on the Internet.',
-    //     ]
-    // },{
-    //     questionNumber : 9,
-    //     question: 'CSS is an acronym for ',
-    //     correctAnswer:'Cascading Style Sheet',
-    //     option:[
-    //         'Cascading Style Sheet',
-    //         'Costume Style Sheet',
-    //         'Cascading System Style',
-    //         'None of the above',
-    //     ]
-    // },{
-    //     questionNumber : 10,
-    //     question: 'Which of the following protocols is not used on the Internet?',
-    //     correctAnswer:'WIRL',
-    //     option:[
-    //         'Gopher',
-    //         'HTTP',
-    //         'WIRL',
-    //         'Telnet',
-    //     ]
-    // },{
-    //     questionNumber : 11,
-    //     question: 'Which of the following HTML tags will insert a line break?',
-    //     correctAnswer:'br',
-    //     option:[
-    //         'p',
-    //         'break',
-    //         'line',
-    //         'br',
-    //     ]
-    // },{
-    //     questionNumber : 12,
-    //     question: 'Which of the following tags are related to a table in HTML?',
-    //     correctAnswer:'table tr td',
-    //     option:[
-    //         'table row column',
-    //         'table tr td',
-    //         'table head body',
-    //         'table header footer',
-    //     ]
-    // },{
-    //     questionNumber : 13,
-    //     question: 'What is the purpose of the tt tag in HTML?',
-    //     correctAnswer:'The tag <tt> is one of the text formatting tags.',
-    //     option:[
-    //         'The tag tt is one of the text formatting tags.',
-    //         'The tag tt is one of the image formatting tags.',
-    //         'The tag tt is one of the table formatting tags.',
-    //         'None of the above',
-    //     ]
-    // },{
-    //     questionNumber : 14,
-    //     question: 'What is the purpose of HTML forms?',
-    //     correctAnswer:'To collect user input.',
-    //     option:[
-    //         'To display the content of an email.',
-    //         'To display animation effect.',
-    //         'To collect user input.',
-    //         'None of the above',
-    //     ]
-    // },{
-    //     questionNumber : 15,
-    //     question: 'What is iframe used for in HTML?',
-    //     correctAnswer:'To display a web page within a web page.',
-    //     option:[
-    //         'To display a web page within a web page.',
-    //         'To display a web page with an animation effect.',
-    //         'To display a web page without a browser.',
-    //         'All the answers are true',
-    //     ]
-    // }
+    {
+        questionNumber : 6,
+        question: 'Can we define the direction of the text via a CSS property?',
+        correctAnswer:'No, its not possible.',
+        option:[
+            'Yes its possible.',
+            'No, its not possible.',
+            'may be possible.',
+            'A true',
+        ]
+    },{
+        questionNumber : 7,
+        question: 'Choose the correct HTML tag for a large title.',
+        correctAnswer:'H1',
+        option:[
+            'H1',
+            'Heading',
+            'Head',
+            'H6',
+        ]
+    },{
+        questionNumber : 8,
+        question: 'In HTML, Uniform Resource Locator (URL) is used ',
+        correctAnswer:'To identify a name or a resource on the Internet.',
+        option:[
+            'To create a frame document.',
+            'To create an image map in a web page.',
+            'To customize the image in a web page.',
+            'To identify a name or a resource on the Internet.',
+        ]
+    },{
+        questionNumber : 9,
+        question: 'CSS is an acronym for ',
+        correctAnswer:'Cascading Style Sheet',
+        option:[
+            'Cascading Style Sheet',
+            'Costume Style Sheet',
+            'Cascading System Style',
+            'None of the above',
+        ]
+    },{
+        questionNumber : 10,
+        question: 'Which of the following protocols is not used on the Internet?',
+        correctAnswer:'WIRL',
+        option:[
+            'Gopher',
+            'HTTP',
+            'WIRL',
+            'Telnet',
+        ]
+    },{
+        questionNumber : 11,
+        question: 'Which of the following HTML tags will insert a line break?',
+        correctAnswer:'br',
+        option:[
+            'p',
+            'break',
+            'line',
+            'br',
+        ]
+    },{
+        questionNumber : 12,
+        question: 'Which of the following tags are related to a table in HTML?',
+        correctAnswer:'table tr td',
+        option:[
+            'table row column',
+            'table tr td',
+            'table head body',
+            'table header footer',
+        ]
+    },{
+        questionNumber : 13,
+        question: 'What is the purpose of the tt tag in HTML?',
+        correctAnswer:'The tag <tt> is one of the text formatting tags.',
+        option:[
+            'The tag tt is one of the text formatting tags.',
+            'The tag tt is one of the image formatting tags.',
+            'The tag tt is one of the table formatting tags.',
+            'None of the above',
+        ]
+    },{
+        questionNumber : 14,
+        question: 'What is the purpose of HTML forms?',
+        correctAnswer:'To collect user input.',
+        option:[
+            'To display the content of an email.',
+            'To display animation effect.',
+            'To collect user input.',
+            'None of the above',
+        ]
+    },{
+        questionNumber : 15,
+        question: 'What is iframe used for in HTML?',
+        correctAnswer:'To display a web page within a web page.',
+        option:[
+            'To display a web page within a web page.',
+            'To display a web page with an animation effect.',
+            'To display a web page without a browser.',
+            'All the answers are true',
+        ]
+    }
 ]
